b691caf074bd99e03e2f9cce71ef89c7a7be06cf
Peter Lee <peterdjlee@gmail.com>
2019-06-24 23:49:46 -0400

    implement post request for a user with id

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ app.use(
     })
 )
 
-app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-})
-
-app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUser)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
